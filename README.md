534f92f8f86e18f401303a9dcac8933c02c10123
metamn <bartus.csongor@gmail.com>
2019-06-28 12:07:18 +0300

    Updating docs

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # styled-components
+
 Playin' with...
+
+It seems styled components are not the ideal way on long term: https://spectrum.chat/styled-components/general/questions-about-object-notation~05d502cc-1b96-4d05-abf7-4353cc9459bd
+
+Instead `styled-jss` might be the solution
--- src/App.js
+++ src/App.js
@@ -2,10 +2,22 @@ import React, { useContext } from "react";
 import styled, { createGlobalStyle } from "styled-components";
 import ThemeContext from "./Theme";
 
-const GlobalStyle = styled.createGlobalStyle(props => ({
+// createGlobalStyle accepts only tagged template literals
+// - https://www.styled-components.com/docs/api#createglobalstyle
+// - this has no effect:
+const GlobalStyle = createGlobalStyle(props => ({
   ...props.theme.colors.default,
   ...props.theme.textStyles.default
 }));
+
+// This does the job
+const GlobalStyle2 = createGlobalStyle`
+  body {
+    font-size: 100%;
+    line-height: 1.25;
+    --lem: 1.25em;
+  }
+`;
 
 const Container = styled.section([], {
   padding: "var(--lem)"
@@ -21,6 +33,7 @@ const App = () => {
   return (
     <>
       <GlobalStyle theme={theme} />
+      <GlobalStyle2 />
       <Container>
         <Title theme={theme}>Styled components test app</Title>
       </Container>
